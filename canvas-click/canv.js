1732936ff201caf32cdb8feaa3be5cb3471f6b25
SetUp <abdulmumeenibroheem51@gmail.com>
2023-08-17 14:51:41 +0100

    checking if line drawn

--- canvas-click/canv.js
+++ canvas-click/canv.js
@@ -2,36 +2,53 @@ const container = document.getElementById("container");
 const canvas = document.getElementById("drawContainer");
 const ctx = canvas.getContext("2d");
 
+const click = document.querySelector("#click");
+const draw = document.querySelector("#draw");
+
 let isDrawing = false;
 let startOffsetX, startOffsetY;
+//endline
+let drawn = false;
 
-container.addEventListener("mousedown", (e) => {
-  isDrawing = true;
-  boundRect = container.getBoundingClientRect();
+click.addEventListener("click", () => {
+  container.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+    boundRect = container.getBoundingClientRect();
 
-  startOffsetX = e.clientX - boundRect.left;
-  startOffsetY = e.clientY - boundRect.top;
+    startOffsetX = e.clientX - boundRect.left;
+    startOffsetY = e.clientY - boundRect.top;
+  });
+
+  container.addEventListener("mousemove", (e) => {
+    if (!isDrawing) return;
+
+    boundRect = container.getBoundingClientRect();
+    endOffsetX = e.clientX - boundRect.left;
+    endOffsetY = e.clientY - boundRect.top;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.moveTo(startOffsetX, startOffsetY);
+    ctx.lineTo(endOffsetX, endOffsetY);
+    ctx.lineWidth = 4;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "red";
+    ctx.stroke();
+  });
+
+  endLine = () => {
+    isDrawing = false;
+    drawn = true;
+  };
+  container.addEventListener("mouseup", endLine);
+  container.addEventListener("mousleave", endLine);
 });
 
-container.addEventListener("mousemove", (e) => {
-  if (!isDrawing) return;
-
-  boundRect = container.getBoundingClientRect();
-  endOffsetX = e.clientX - boundRect.left;
-  endOffsetY = e.clientY - boundRect.top;
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  ctx.moveTo(startOffsetX, startOffsetY);
-  ctx.lineTo(endOffsetX, endOffsetY);
-  ctx.lineWidth = 4;
-  ctx.lineCap = "round";
-  ctx.strokeStyle = "white";
-  ctx.stroke();
+//count functioning
+draw.addEventListener("click", () => {
+  if (!drawn) {
+    alert("please draw the ridge line to use this function");
+  } else {
+    alert("Targeted!");
+  }
 });
-
-endLine = () => {
-  isDrawing = false;
-};
-container.addEventListener("mouseup", endLine);
-container.addEventListener("mousleave", endLine);
