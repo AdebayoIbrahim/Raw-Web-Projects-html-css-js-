0f736e74283265ca46f89af8d97bd91e677a6273
SetUp <abdulmumeenibroheem51@gmail.com>
2023-08-19 02:35:54 +0100

    added include characters

--- Password_generator/password.js
+++ Password_generator/password.js
@@ -1,16 +1,45 @@
 const ouput = document.querySelector(".output");
 const gen = document.querySelector("button");
 const length = document.querySelector("#len");
+const numb = document.getElementById("num");
+const upper = document.getElementById("upper");
+const lower = document.getElementById("lower");
+const symb = document.getElementById("symb");
 
-function getPassword(len) {
-  let arr = [];
+//symbols
+
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "0123456789";
+const symbolChars = "!@#$%^&*()_-+=<>?";
+
+function getPassword(len, incLower, incUpper, incSymb, incNum) {
+  let allchar = "";
+  incLower && (allchar += lowercaseChars);
+  incUpper && (allchar += uppercaseChars);
+  incSymb && (allchar += symbolChars);
+  incNum && (allchar += numberChars);
+  let password = "";
   for (i = 0; i < len; i++) {
-    const calculated = Math.floor(Math.random() * 10);
-    arr = [...arr, calculated];
+    const random = Math.floor(Math.random() * allchar.length);
+    password += allchar[random];
   }
-  ouput.innerHTML = arr.join("");
+
+  ouput.textContent = password;
 }
+
 gen.addEventListener("click", () => {
   const lengthVal = Number(length.value);
-  getPassword(lengthVal);
+  const includeLowercase = lower.checked;
+  const includeUppercase = upper.checked;
+  const includeNumbers = numb.checked;
+  const includeSymbols = symb.checked;
+
+  getPassword(
+    lengthVal,
+    includeLowercase,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
 });
