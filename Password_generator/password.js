9babe9edf4dc8f47d628f7ec311598dee9d8e4ab
SetUp <abdulmumeenibroheem51@gmail.com>
2023-08-19 01:14:54 +0100

    generate button functionality

---
+++ Password_generator/password.js
@@ -0,0 +1,16 @@
+const ouput = document.querySelector(".output");
+const gen = document.querySelector("button");
+const length = document.querySelector("#num");
+
+function getPassword(len) {
+  let arr = [];
+  for (i = 0; i < len; i++) {
+    const calculated = Math.floor(Math.random() * 10);
+    arr = [...arr, calculated];
+  }
+  ouput.innerHTML = arr.join("");
+}
+gen.addEventListener("click", () => {
+  const lengthVal = Number(length.value);
+  getPassword(lengthVal);
+});
