59e54efe012a5f5f7dff8ad66f0b6ad9e7d60e13
SetUp <abdulmumeenibroheem51@gmail.com>
2023-08-21 18:40:41 +0100

    added color functionality

--- Drawing-app/draw.js
+++ Drawing-app/draw.js
@@ -3,9 +3,12 @@ const ctx = canvas.getContext("2d");
 const unit = document.getElementById("unit");
 const slider = document.getElementById("slider");
 const clear = document.querySelector(".Clear");
+const color = document.querySelector("#color");
 let isdrawing = false;
 let startOffsetX, startOffsetY;
 let size = 10;
+color.value = "#f345d6";
+let Color = color.value;
 
 slider.addEventListener("change", (e) => {
   val = e.target.value;
@@ -35,14 +38,14 @@ function drawCircle(x, y) {
   let c = Math.PI;
   ctx.beginPath();
   ctx.arc(x, y, size, 0, 2 * c);
-  ctx.fillStyle = "blue";
+  ctx.fillStyle = Color;
   ctx.fill();
 }
 function drawLine(x, y, x2, y2) {
   ctx.beginPath();
   ctx.moveTo(x, y);
   ctx.lineTo(x2, y2);
-  ctx.strokeStyle = "blue";
+  ctx.strokeStyle = Color;
   ctx.lineWidth = `${size * 2}`;
   ctx.stroke();
 }
@@ -52,6 +55,9 @@ endDraw = () => {
 };
 clear.addEventListener("click", () => {
   isdrawing || ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+color.addEventListener("change", (e) => {
+  Color = e.target.value;
 });
 canvas.addEventListener("mouseup", endDraw);
 canvas.addEventListener("mouseleave", endDraw);
