a7476d461c94afc8baf15abe253fa6bdc46df0ad
SetUp <abdulmumeenibroheem51@gmail.com>
2023-08-25 12:55:56 +0100

    percentage functionality

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,15 +1,12 @@
 const display = document.querySelector(".result");
 const buttons = document.querySelectorAll(".button-group button");
 
-let currentInput = ""; // To store the current input expression
-let shouldResetDisplay = false; // To track if the display needs to be reset
-
-// Helper function to update the display
+let currentInput = "";
+let shouldResetDisplay = false;
 function updateDisplay(content) {
   display.textContent = content;
 }
 
-// Helper function to handle number and operator button clicks
 function handleButtonClick(value) {
   if (shouldResetDisplay) {
     currentInput = "";
@@ -19,17 +16,9 @@ function handleButtonClick(value) {
   updateDisplay(currentInput);
 }
 
-// ... Your existing code ...
-
-// Add click event listener to the toggle sign button
-
-// ... Rest of your code ...
-
-// Add click event listener to each button
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const buttonText = button.textContent;
-    // let num = display.textContent;
     switch (buttonText) {
       case "+/-":
         if (currentInput !== "0" && currentInput !== "Error") {
@@ -46,7 +35,8 @@ buttons.forEach((button) => {
         shouldResetDisplay = false;
         updateDisplay("0");
         break;
-
+      case "%":
+        currentInput = currentInput / 100;
       case "=":
         try {
           const result = evaluateExpression(currentInput);
@@ -64,18 +54,6 @@ buttons.forEach((button) => {
   });
 });
 
-// toggleSignButton.addEventListener("click", () => {
-//   if (currentInput !== "0" && currentInput !== "Error") {
-//     if (currentInput.charAt(0) === "-") {
-//       currentInput = currentInput.slice(1); // Remove the minus sign
-//     } else {
-//       currentInput = "-" + currentInput; // Add the minus sign
-//     }
-//     updateDisplay(currentInput);
-//   }
-// });
-
-// Function to safely evaluate the expression
 function evaluateExpression(expression) {
   return new Function("return " + expression)();
 }
